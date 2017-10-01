902f73a37e960b280739c21a592132a4abb1838d
jan <jan@mrozilla.cz>
2017-10-01 22:43:46 +0200

    add works

--- src/content/index.js
+++ src/content/index.js
@@ -2,6 +2,7 @@
 // export
 // =============================================================================
 
+/* eslint-disable max-len */
 module.exports = {
   mainMenu: [
     { title: '👷 Work', payload: 'WORK_PAYLOAD' },
@@ -9,4 +10,42 @@ module.exports = {
     { title: '📝 Blog', payload: 'BLOG_PAYLOAD' },
     { title: '🤔 About', payload: 'ABOUT_PAYLOAD' },
   ],
+  work: [
+    {
+      title:    'hiMoment',
+      image:    'https://mrozilla.cz/static/media/work-himoment.1d1228de.jpg',
+      subtitle: 'your digital happiness guru',
+      url:      'https://medium.com/mrozilla/designing-for-happiness-1db11a1f3952',
+      type:     ['case', 'design', 'dev', 'copy'],
+      tags:     ['branding', 'design', 'website', 'app', 'copywriting'],
+    },
+    {
+      title:    "Mov'in Europe",
+      image:    'https://mrozilla.cz/static/media/work-movin_europe.6af08194.jpg',
+      subtitle: 'mobility is a lifestyle',
+      url:
+        'https://medium.com/mrozilla/branding-an-inter-national-mobility-campaign-38a2260e0ad7',
+      type: ['case', 'design', 'dev', 'copy'],
+      tags: ['branding', 'design', 'website', 'copywriting'],
+    },
+    {
+      title:    'Erasmus Student Network',
+      image:    'https://mrozilla.cz/static/media/work-esn.ce49cadb.jpg',
+      subtitle: 'unity in diversity',
+      url:
+        'https://medium.com/mrozilla/new-visual-style-for-30-000-people-ff5e61d76211',
+      type: ['case', 'design', 'dev', 'copy'],
+      tags: ['design', 'website', 'copywriting'],
+    },
+    {
+      title:    'Vodafone Idea of the Year',
+      image:    'https://mrozilla.cz/static/media/work-napad_roku.d33aed30.jpg',
+      subtitle: 'helping ideas grow',
+      url:
+        'https://medium.com/mrozilla/refreshing-a-startup-competition-21f09348a888',
+      type: ['case', 'design', 'dev'],
+      tags: ['design', 'website'],
+    },
+  ],
 };
+/* eslint-enable */
