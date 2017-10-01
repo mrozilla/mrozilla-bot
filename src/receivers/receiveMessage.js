2fff2003bccf65b40f2421f1f79068b82962efc3
jan <jan@mrozilla.cz>
2017-10-01 21:52:05 +0200

    remove payload check for quick replies

--- src/receivers/receiveMessage.js
+++ src/receivers/receiveMessage.js
@@ -15,6 +15,8 @@ module.exports = async function receiveMessage({ message, sender }) {
     console.log(`Received message from sender.id: ${sender.id}`);
     console.log(`Message is: ${JSON.stringify(message)}`);
 
+    console.log(`Quick reply payload is: ${message.quick_reply.payload}`);
+
     if (message.text) {
       goWTF(sender.id);
       return;
@@ -26,27 +28,27 @@ module.exports = async function receiveMessage({ message, sender }) {
     }
 
     if (message.quick_reply) {
-      if (message.quick_reply.payload) {
-        if (message.quick_reply.payload === 'WORK_PAYLOAD') {
-          await sendText(sender.id, 'This is my work!');
-          return;
-        }
-
-        if (message.quick_reply.payload === 'LAB_PAYLOAD') {
-          await sendText(sender.id, 'This is my lab!');
-          return;
-        }
-
-        if (message.quick_reply.payload === 'BLOG_PAYLOAD') {
-          await sendText(sender.id, 'This is my blog!');
-          return;
-        }
-
-        if (message.quick_reply.payload === 'ABOUT_PAYLOAD') {
-          await sendText(sender.id, 'This is my about!');
-          return;
-        }
+      if (message.quick_reply.payload === 'WORK_PAYLOAD') {
+        await sendText(sender.id, 'This is my work!');
+        return;
       }
+
+      if (message.quick_reply.payload === 'LAB_PAYLOAD') {
+        await sendText(sender.id, 'This is my lab!');
+        return;
+      }
+
+      if (message.quick_reply.payload === 'BLOG_PAYLOAD') {
+        await sendText(sender.id, 'This is my blog!');
+        return;
+      }
+
+      if (message.quick_reply.payload === 'ABOUT_PAYLOAD') {
+        await sendText(sender.id, 'This is my about!');
+        return;
+      }
+
+      goWTF(sender.id);
     }
 
     goWTF(sender.id);
