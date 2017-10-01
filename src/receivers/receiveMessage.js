4bde65ef1bd7b0c058bcaacfcdf116f4b3dba496
jan <jan@mrozilla.cz>
2017-10-01 21:16:06 +0200

    add quick reply responses

--- src/receivers/receiveMessage.js
+++ src/receivers/receiveMessage.js
@@ -3,24 +3,52 @@
 // =============================================================================
 
 // components
+const goWTF = require('../stories/goWTF');
 const sendText = require('../senders/sendText');
 
 // =============================================================================
 // component
 // =============================================================================
 
-module.exports = async function handleMessage({ message, event }) {
-  if (!event.message.is_echo) {
-    console.log(`Received message from sender.id: ${event.sender.id}`);
+module.exports = async function receiveMessage({ message, sender }) {
+  if (!message.is_echo) {
+    console.log(`Received message from sender.id: ${sender.id}`);
     console.log(`Message is: ${JSON.stringify(message)}`);
 
     if (message.text) {
-      await sendText(event.sender.id, `"Your text was: ${message.text}"`);
-    } else if (message.attachments) {
-      await sendText(
-        event.sender.id,
-        "Sorry, I can't deal with attachments just yet 🤷‍♂️",
-      );
+      goWTF(sender.id);
+      return;
     }
+
+    if (message.attachments) {
+      goWTF(sender.id);
+      return;
+    }
+
+    if (message.quick_reply) {
+      if (message.quick_reply.payload) {
+        if (message.quick_reply.payload === 'WORK_PAYLOAD') {
+          await sendText(sender.id, 'This is my work!');
+          return;
+        }
+
+        if (message.quick_reply.payload === 'LAB_PAYLOAD') {
+          await sendText(sender.id, 'This is my lab!');
+          return;
+        }
+
+        if (message.quick_reply.payload === 'BLOG_PAYLOAD') {
+          await sendText(sender.id, 'This is my blog!');
+          return;
+        }
+
+        if (message.quick_reply.payload === 'ABOUT_PAYLOAD') {
+          await sendText(sender.id, 'This is my about!');
+          return;
+        }
+      }
+    }
+
+    goWTF(sender.id);
   }
 };
--- src/receivers/receivePostback.js
+++ src/receivers/receivePostback.js
@@ -2,11 +2,9 @@
 // import
 // =============================================================================
 
-// components
-const sendText = require('../senders/sendText');
-
 // stories
 const greetUser = require('../stories/greetUser');
+const goWTF = require('../stories/goWTF');
 
 // =============================================================================
 // component
@@ -18,17 +16,7 @@ module.exports = async function handlePostback({
 }) {
   if (payload === 'GET_STARTED_PAYLOAD') {
     greetUser(sender.id);
-  }
-  if (payload === 'WORK_PAYLOAD') {
-    await sendText(sender.id, 'This is my work!');
-  }
-  if (payload === 'LAB_PAYLOAD') {
-    await sendText(sender.id, 'This is my lab!');
-  }
-  if (payload === 'BLOG_PAYLOAD') {
-    await sendText(sender.id, 'This is my blog!');
-  }
-  if (payload === 'ABOUT_PAYLOAD') {
-    await sendText(sender.id, 'This is my about!');
+  } else {
+    goWTF(sender.id);
   }
 };
